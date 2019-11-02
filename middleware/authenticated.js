4d307b3a81b37d52220783d5a824d0c1861e9f94
aman <amanwilk@gmail.com>
2019-11-02 00:08:16 -0400

    adding new looks

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,9 +2,8 @@ export default function ({store, redirect, query, route}) {
   const token = store.state.payload.token;
   const loginSuccess = query.success;
   if (loginSuccess) {
-    alert(JSON.stringify(query));
     store.commit("payload/setAuthenticated", true);
-    return redirect('/')("/graphs");
+    return redirect("/graphs");
   }
   if (!token) {
     alert("Signin Please");
